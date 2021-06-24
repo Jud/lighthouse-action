57682858685308a2a114556dd434de2282fb9c57
Judson Stephenson <Jud@users.noreply.github.com>
2021-06-23 21:43:22 -0500

    Allow passing an auth cookie via environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,14 @@ OUTPUT_FILENAME=$(echo "$REPORT_URL" | sed 's/[^a-zA-Z0-9]/_/g')
 OUTPUT_PATH="$GITHUB_WORKSPACE/$OUTPUT_FOLDER/$OUTPUT_FILENAME"
 mkdir -p "$OUTPUT_FOLDER"
 
+# Set the auth cookie param if passed
+[ -z "${AUTH_COOKIE+x}" ] || AUTH_COOKIE=--extra-headers=\"{\"cookie\":\"$AUTH_COOKIE\"}\"
+
 # Clarify in logs which URL we're auditing.
 printf "* Beginning audit of %s ...\n\n" "$REPORT_URL"
 
 # Run Lighthouse!
-lighthouse --port=9222 --chrome-flags="--headless --disable-gpu --no-sandbox --no-zygote" --output "html" --output "json" --output-path "${OUTPUT_PATH}" "${REPORT_URL}"
+lighthouse --port=9222 --chrome-flags="--headless --disable-gpu --no-sandbox --no-zygote" $AUTH_COOKIE --output "html" --output "json" --output-path "${OUTPUT_PATH}" "${REPORT_URL}"
 
 # Parse individual scores from JSON output.
 # Unorthodox jq syntax because of dashes -- https://github.com/stedolan/jq/issues/38
