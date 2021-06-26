90c7707cee775e588dc6772db80adfd4d15c8e09
Jud <Jud.Stephenson@gmail.com>
2021-06-26 03:24:22 +0000

    Add bundle size to output

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,12 +33,15 @@ SCORE_ACCESSIBILITY=$(jq '.categories["accessibility"].score' "$OUTPUT_PATH".rep
 SCORE_PRACTICES=$(jq '.categories["best-practices"].score' "$OUTPUT_PATH".report.json)
 SCORE_SEO=$(jq '.categories["seo"].score' "$OUTPUT_PATH".report.json)
 SCORE_PWA=$(jq '.categories["pwa"].score' "$OUTPUT_PATH".report.json)
+BUNDLE_SIZE=$(cat "$OUTPUT_PATH".report.json | jq '.audits."network-requests".details.items[] | select(.url|test("netlify.app/.*js$")) | .resourceSize' | awk '{ SUM += $1} END { print SUM }' | awk '{$1/=1024;printf "%.2fKB\n",$1}' | awk '{$1/=1024;printf "%.2fMB\n",$1}')
 
 # Print scores to standard output (0 to 100 instead of 0 to 1).
 # Using hacky bc b/c bash hates floating point arithmetic...
 printf '\n* Completed audit of %s ! Scores are printed below:\n\n' "$REPORT_URL"
 printf -v RUN_OUTPUT '+-------------------------------+\n'
 printf -v RUN_OUTPUT '%s|  Performance:           %.0f\t|\n' "$RUN_OUTPUT" "$(echo "$SCORE_PERFORMANCE*100" | bc -l)"
+printf -v RUN_OUTPUT '%s|  Bundle Size:           %s\t|\n' "$RUN_OUTPUT" "$(echo "$BUNDLE_SIZE" | bc -l)"
+
 #printf "|  Accessibility:         %.0f\t|\n" "$(echo "$SCORE_ACCESSIBILITY*100" | bc -l)"
 #printf "|  Best Practices:        %.0f\t|\n" "$(echo "$SCORE_PRACTICES*100" | bc -l)"
 #printf "|  SEO:                   %.0f\t|\n" "$(echo "$SCORE_SEO*100" | bc -l)"
