5033a202b3f811b9e6649ffd533b746cb76690ad
Jud <Jud.Stephenson@gmail.com>
2021-06-28 17:40:16 +0000

    Fixup path variable

--- lighthouse.js
+++ lighthouse.js
@@ -39,7 +39,7 @@ async function generate_report_for(url, browser, password) {
   return await lighthouse(url, {onlyCategories: ['performance'], port: PORT, disableStorageReset: true})
 }
 
-function write_report(json, html, PATH) {
+function write_report(json, html, path) {
   fs.writeFileSync(`${path}.report.html`, html);
   fs.writeFileSync(`${path}.report.json`, json.report);
 }
